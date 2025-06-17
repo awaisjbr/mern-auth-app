ac2cebe31f4606926c92777727f2756b845a9cfe
awaisjbr <awaisjbr@gmail.com>
2025-06-17 14:35:24 +0500

    17-06-2025 3

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json({limit: "5mb"}));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ['https://mern-auth-app-kohl.vercel.app' , 'http://localhost:4000'],
     credentials: true
 }))
 app.use("/api/auth", authRouter);
